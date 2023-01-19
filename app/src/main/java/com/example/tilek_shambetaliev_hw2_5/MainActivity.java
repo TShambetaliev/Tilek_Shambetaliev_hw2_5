fc66e26e557b874896be93cd75629598bc020f3e
Tshambetaliev <tshambetaliev@gmail.com>
2023-01-19 23:28:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/tilek_shambetaliev_hw2_5/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tilek_shambetaliev_hw2_5;
+
+import static com.example.tilek_shambetaliev_hw2_5.R.layout.activity_main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+    }
+}
